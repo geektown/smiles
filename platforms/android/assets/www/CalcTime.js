50c6c6c23d5ec71deee6de1cedfc1f86b8e2ca9a
geektown <156575785@qq.com>
2015-08-02 13:54:25 +0800

    use cordova latest version and code structures

---
+++ platforms/android/assets/www/CalcTime.js
@@ -0,0 +1,32 @@
+﻿ /* CalcTime.js */ 
+ 
+ $( document ).ready(function() {
+	
+	setTimeout(calcTime,1000);
+ 
+});
+
+var totalTime = 2999024919000;
+function calcTime() 
+{ 
+	
+	var nowtime = new Date().getTime();        //今天的日期(毫秒值)
+    var youtime = totalTime-nowtime;//还有多久(毫秒值)
+    var seconds = youtime/1000;
+    var minutes = Math.floor(seconds/60);
+    var hours = Math.floor(minutes/60);
+    var days = Math.floor(hours/24);
+	var months = Math.floor(days/30);
+	var years = Math.floor(months/12);
+	
+	var Cyear = years;
+	var CMonth = months % 12;
+    var CDay= days % 30 ;
+    var CHour= hours % 24;
+    var CMinute= minutes % 60;
+    var CSecond= Math.floor(seconds%60);//"%"是取余运算，可以理解为60进一后取余数，然后只要余数。
+	
+	$("#remainingTime").html(Cyear + "年" + CMonth + "月" + CDay+"天" +"</br>"+ CHour+"时"+CMinute+"分"+CSecond+"秒"); 
+	
+	setTimeout(calcTime,1000);
+}
---
+++ www/myRating.js
@@ -0,0 +1,43 @@
+﻿ $( document ).ready(function() {
+	
+$('#star_iq').raty({
+  size     	: 24,
+  number	: 5,
+  width		: 120,
+  target	: "#rating1Hint",
+  targetKeep: true,
+  targetText: '--',
+  starOff  	: 'img/star-off-big.png',
+  starOn   	: 'img/star-on-big.png'
+});
+
+
+$('#star_emotion').raty({
+  iconRange: [
+    { range: 1, on: 'img/1.png', off: 'img/0.png' },
+    { range: 2, on: 'img/2.png', off: 'img/0.png' },
+    { range: 3, on: 'img/3.png', off: 'img/0.png' },
+    { range: 4, on: 'img/4.png', off: 'img/0.png' },
+    { range: 5, on: 'img/5.png', off: 'img/0.png' }
+  ],
+   width		: 120,
+  target	: "#star_emotionHint",
+  targetKeep: true,
+  targetText: '--',
+  hints: ['极度悲伤', '很差', '一般般', '很好', '好的一米']
+});
+
+$('#star_sports').raty({
+  size     	: 24,
+  number	: 5,
+  width		: 120,
+  target	: "#star_sportsHints",
+  targetKeep: true,
+  targetText: '--',
+  starOff  	: 'img/star-off-big.png',
+  starOn   	: 'img/star-on-big.png',
+  hints: ['一动不动', '随处走走', '一般运动', '大汗淋淋', '过度锻炼']
+});
+
+});
+
